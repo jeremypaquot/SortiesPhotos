51b37bf3c390251b8d2babd013e1f3dcf67c4c78
jeremypaquot <jeremypaquotdesign@gmail.com>
2026-08-28 10:27:25 +0200

    chore: ajout de src/memory.ts

---
+++ src/memory.ts
@@ -0,0 +1,3 @@
+import type{Outing}from'./types.js';
+export class MemoryOutings{private items:Outing[];constructor(seed:Outing[]=[]){this.items=structuredClone(seed)}async list(){return structuredClone(this.items)}async save(o:Outing){this.items=[...this.items.filter(x=>x.id!==o.id),structuredClone(o)];return o}async remove(id:string){this.items=this.items.filter(x=>x.id!==id)}}
+
